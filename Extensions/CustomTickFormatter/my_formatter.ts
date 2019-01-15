dfc562b04b93e1741dc1723911c6da48756a384b
ARTUSI <xavier.artusi@cea.fr>
2019-01-15 10:21:40 +0100

    Format ticker extension

--- Extensions/CustomTickFormatter/my_formatter.ts
+++ Extensions/CustomTickFormatter/my_formatter.ts
@@ -1,16 +1,16 @@
-import { TickFormatter } from "models/formatters/tick_formatter"
+import {TickFormatter} from "models/formatters/tick_formatter"
 
-export class MyFormatter extends TickFormatter{
-  type: "MyFormatter"
+export class MyFormatter extends TickFormatter {
+  type = "MyFormatter"
 
   // TickFormatters should implement this method, which accepts a list
   // of numbers or strings (ticks) and returns a list of strings
-  doFormat(ticks: string[] | number[]): string[]{
+  doFormat(ticks: string[] | number[]): string[] {
     // format the first tick as-is
     const formatted = [`${ticks[0]}`]
     for (let i = 1, len = ticks.length; i < len; i++) {
-      formatted.push(`+${(Number(ticks[i])-Number(ticks[0])).toPrecision(2)}`)
+      formatted.push(`+${(Number(ticks[i]) - Number(ticks[0])).toPrecision(2)}`)
     }
-    return formatted 
+    return formatted
   }
 }
