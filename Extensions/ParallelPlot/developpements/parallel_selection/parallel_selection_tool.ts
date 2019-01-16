11024c5fd8b2d0e02ed3964f46b9fa7eb9c611a6
ARTUSI <xavier.artusi@cea.fr>
2019-01-16 17:37:26 +0100

    Selections pan drag and delete glyphs ok.. TODO : select data then resize

--- Extensions/ParallelPlot/developpements/parallel_selection/parallel_selection_tool.ts
+++ Extensions/ParallelPlot/developpements/parallel_selection/parallel_selection_tool.ts
@@ -3,8 +3,8 @@ import {BoxSelectTool, BoxSelectToolView} from "models/tools/gestures/box_select
 import {Rect} from "models/glyphs/rect"
 import {ColumnDataSource} from "models/sources/column_data_source"
 import {GlyphRenderer} from "models/renderers/glyph_renderer"
-import {ColumnarDataSource, MultiLine, Scale, Selection} from "models"
-import {TapEvent, MoveEvent, GestureEvent} from "core/ui_events"
+import {ColumnarDataSource, MultiLine, Scale} from "models"
+import {MoveEvent, GestureEvent, TapEvent} from "core/ui_events"
 
 export interface HasRectCDS {
     glyph: Rect
@@ -18,18 +18,27 @@ export interface HasMultiLineCDS {
 
 type SelectionMode = "add" | "resize" | "drag"
 
+type BoxParams = {
+    x: number
+    y: number
+    h: number
+    w: number
+}
+
 export class ParallelSelectionView extends BoxSelectToolView {
     model: ParallelSelectionTool
     private xscale: Scale
     private yscale: Scale
     private xdata: number[]
     private ydata: number[][]
-    private selected: Selection
     private cds_select: ColumnDataSource
     private cds_data: ColumnDataSource
     private glyph_select: Rect
     private glyph_data: MultiLine
     private selection_mode: SelectionMode
+    private ind_active_box: null | number
+    private panning: boolean = false
+    private _base_box_parameters: BoxParams | null
 
     initialize(options: any): void {
         super.initialize(options)
@@ -59,43 +68,20 @@ export class ParallelSelectionView extends BoxSelectToolView {
         const [xskey, yskey] = [(this.glyph_data as any).xs.field, (this.glyph_data as any).ys.field]
         this.xdata = this.cds_data.get_array(xskey)[0] as number[]
         this.ydata = this.cds_data.get_array(yskey)
-        this.selected = this.cds_data.selected
 
         this.connect(this.plot_model.frame.x_ranges[this.model.renderer_select.x_range_name].change,
-            () => this._resize_boxes())
+            () => this._resize_boxes_on_zoom())
     }
-
-
 
     get _box_width(): number {
         return this.xscale.invert(this.model.box_width) - this.xscale.invert(0)
     }
 
-    _resize_boxes(): void {
-        //resize selection boxes when zooming to keep a constant (pixel) size
-        const cds = this.cds_select
-        const array_width = cds.get_array((this.glyph_select as any).width.field)
-        const new_width = this._box_width
-        array_width.forEach((_, i) => array_width[i] = new_width)
-        this._emit_cds_changes(cds)
-    }
-
-    _tap(_ev: TapEvent) {
-        console.log('Tap')
-    }
-
-    _pan(ev: GestureEvent) {
-        switch (this.selection_mode) {
-            case "add": {
-                super._pan(ev)
-            }
-            case "drag": {}
-            case "resize": {}
-        }
-    }
-
-    _move(_ev: MoveEvent) {
-        //Switch mode
+    get _cds_select_keys() {
+        const glyph_select: any = this.glyph_select
+        const [xkey, ykey] = [glyph_select.x.field, glyph_select.y.field]
+        const [wkey, hkey] = [glyph_select.width.field, glyph_select.height.field]
+        return {xkey, ykey, wkey, hkey}
     }
 
     _emit_cds_changes(cds: ColumnarDataSource, redraw: boolean = true,
@@ -107,6 +93,144 @@ export class ParallelSelectionView extends BoxSelectToolView {
         if (emit) {
             cds.data = cds.data
             cds.properties.data.change.emit()
+        }
+    }
+
+    _box_paramaters(index: number) {
+        const {xkey, ykey, wkey, hkey} = this._cds_select_keys
+        const x = this.cds_select.get_array<number>(xkey)[index]
+        const y = this.cds_select.get_array<number>(ykey)[index]
+        const w = this.cds_select.get_array<number>(wkey)[index]
+        const h = this.cds_select.get_array<number>(hkey)[index]
+        return {x, y, w, h}
+    }
+
+    _hit_test_boxes(sx: number, sy: number): number | null {
+        const nboxes = this.cds_select.get_length()
+        if (nboxes != 0 && nboxes != null) {
+            const [xtest, ytest] = [this.xscale.invert(sx), this.yscale.invert(sy)]
+            for (let i = 0; i < nboxes; i++) {
+                const {x, y, w, h} = this._box_paramaters(i)
+                if (xtest >= (x - w / 2) && xtest <= x + w / 2 &&
+                    ytest >= (y - h / 2) && ytest <= y + h / 2) {
+                    return i
+                }
+            }
+            return null
+        } else {
+            return null
+        }
+    }
+
+    _resize_boxes_on_zoom() {
+        //resize selection boxes when zooming to keep a constant (pixel) size
+        const cds = this.cds_select
+        const array_width = cds.get_array((this.glyph_select as any).width.field)
+        const new_width = this._box_width
+        array_width.forEach((_, i) => array_width[i] = new_width)
+        this._emit_cds_changes(cds, true, false, false)
+    }
+
+    _drag_start(ev: GestureEvent) {
+        //Save y position of the drag start
+        if (this.ind_active_box != null) {
+            this._base_point = [this.xscale.invert(ev.sx), this.yscale.invert(ev.sy)]
+            this._base_box_parameters = this._box_paramaters(this.ind_active_box)
+        }
+    }
+
+    _update_box_vpos(index_box: number, delta_y: number) {
+        if (this._base_box_parameters != null) {
+            const cds = this.cds_select
+            const {ykey} = this._cds_select_keys
+            const {y: current_y, h} = this._base_box_parameters
+            let new_y = current_y + delta_y
+            new_y = new_y - Math.max(0, (new_y + h / 2) - 1) - Math.min(0, (new_y - h / 2))
+            cds.get_array<number>(ykey)[index_box] = new_y
+            this._emit_cds_changes(cds, true, false, false)
+        }
+    }
+
+    _drag(ev: GestureEvent) {
+        if (this.ind_active_box != null && this._base_point != null) {
+            const delta_y = this.yscale.invert(ev.sy) - this._base_point[1]
+            this._update_box_vpos(this.ind_active_box, delta_y)
+        }
+    }
+
+    _drag_stop(_ev: GestureEvent) {
+        this._base_point = null
+        this._base_box_parameters = null
+    }
+
+    _pan_start(ev: GestureEvent) {
+        this.panning = true
+        switch (this.selection_mode) {
+            case "add": {
+                super._pan_start(ev)
+                break
+            }
+            case "drag": {
+                this._drag_start(ev)
+                break
+            }
+            case "resize": {
+                break
+            }
+        }
+    }
+
+    _pan(ev: GestureEvent) {
+        switch (this.selection_mode) {
+            case "add": {
+                super._pan(ev)
+                break
+            }
+            case "drag": {
+                this._drag(ev)
+                break
+            }
+            case "resize": {
+                break
+            }
+        }
+    }
+
+    _pan_end(ev: GestureEvent) {
+        switch (this.selection_mode) {
+            case "add": {
+                super._pan_end(ev)
+                break
+            }
+            case "drag": {
+                this._drag_stop(ev)
+                break
+            }
+            case "resize": {
+                break
+            }
+        }
+        this.panning = false
+    }
+
+    _move(ev: MoveEvent) {
+        //Switch mode
+        if (this.panning) {return }
+        this.ind_active_box = this._hit_test_boxes(ev.sx, ev.sy)
+        if (this.ind_active_box != null) {
+            this.selection_mode = "drag"
+        } else {
+            this.selection_mode = "add"
+        }
+    }
+
+    _doubletap(_ev: TapEvent) {
+        //delete box on double tap
+        if (this.ind_active_box != null) {
+            this.cds_select.columns().forEach(key => {
+                this.cds_select.get_array(key).splice((this.ind_active_box as any), 1)
+            })
+            this._emit_cds_changes(this.cds_select)
         }
     }
 
@@ -130,31 +254,18 @@ export class ParallelSelectionView extends BoxSelectToolView {
         }
     }
 
-    _update_selection(x_indices: number[],
-        [y0, y1]: [number, number]): void {
-
-        const ind_axis_sel = this._set_of_selected_indices(x_indices, [y0, y1])
-    }
-
-    _set_selection_box(xs: number[], [y0, y1]: [number, number]): void {
-
-        // Type once dataspecs are typed
-
+    _set_box_select(xs: number[], [y0, y1]: [number, number]): void {
         const y = (y0 + y1) / 2.
         const w = this._box_width
         const h = Math.min(1, y1) - Math.max(0, y0)
 
-        const glyph_select: any = this.glyph_select
-        const [xkey, ykey] = [glyph_select.x.field, glyph_select.y.field]
-        const [wkey, hkey] = [glyph_select.width.field, glyph_select.height.field]
-
+        const {xkey, ykey, wkey, hkey} = this._cds_select_keys
         xs.forEach(x => {
             if (xkey) this.cds_select.get_array(xkey).push(x)
             if (ykey) this.cds_select.get_array(ykey).push(y)
             if (wkey) this.cds_select.get_array(wkey).push(w)
             if (hkey) this.cds_select.get_array(hkey).push(h)
         })
-
         this._emit_cds_changes(this.cds_select)
     }
 
@@ -169,8 +280,9 @@ export class ParallelSelectionView extends BoxSelectToolView {
         const xs = x_indices.reduce((a: number[], i) => a.concat(this.xdata[i]), [])
 
         // this._update_selection([x0, x1], [y0, y1])
-        this._set_selection_box(xs, [y0, y1])
+        this._set_box_select(xs, [y0, y1])
     }
+
 }
 
 export class ParallelSelectionTool extends BoxSelectTool {
--- Extensions/ParallelPlot/developpements/parallel_selection/test_parallel_plot.py
+++ Extensions/ParallelPlot/developpements/parallel_selection/test_parallel_plot.py
@@ -5,8 +5,8 @@ from bokeh.models import ColumnDataSource
 
 from parallel_selection_tool import ParallelSelectionTool
 
-npts = 10
-ndims = 2
+npts = 100
+ndims = 10
 
 data_source = ColumnDataSource(dict(
     xs=(1 + np.arange(ndims))[None, :].repeat(npts, axis=0).tolist(),
@@ -21,10 +21,10 @@ p = figure(x_range=(0.5, ndims+0.5), y_range=(0, 1), width=1000)
 parallel_renderer = p.multi_line(xs="xs", ys="ys", source=data_source,
                                  line_color="#8073ac", line_width=1)
 
-selection_renderer = p.rect('x', 'y', 'width', 'height', source=rect_source, alpha=0.3)
+selection_renderer = p.rect('x', 'y', 'width', 'height', source=rect_source, alpha=0.7)
 selection_tool = ParallelSelectionTool(
     renderer_select=selection_renderer, renderer_data=parallel_renderer,
-    box_width=30)
+    box_width=10)
 
 p.add_tools(selection_tool)
 p.toolbar.active_drag = selection_tool
